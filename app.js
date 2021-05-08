a1198d4630093494ecc731d815cd22d5f3d71d18
Przemysław Kolczyński <przekolczynski@gmail.com>
2021-05-08 15:29:59 +0200

    added initial db connection

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+// Importing modules
 const express = require('express');
+const { Sequelize } = require('sequelize');
+
+// Initializing app with needed third-party modules
 const app = express();
 const port = 3000
-
 const helmet = require('helmet');
 app.use(helmet());
-
-
 var bodyParser = require('body-parser');
+
+// Setting up DB
+const sequelize = new Sequelize('mysql://admin:admin@localhost:3307/restaurantdb')
+
+// Test DB connection
+const testDb = (sequelize) => {
+    sequelize
+      .authenticate()
+      .then((result) => {
+        console.log('testDb -> result', result);
+      })
+      .catch((error) => {
+        console.log('testDb -> error', error);
+      });
+  };
+
+  testDb(sequelize);
+
+  
+
 
 
 app.listen(port, () => {
